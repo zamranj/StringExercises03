3d8074c5c147156de9a874bd3c8ef91a0a4dc0ec
zeejay21 <>
2021-06-05 19:22:00 +0000

    Fixed dna exercise dna now prints with no white spaces and all caps

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
 
 // First, print out the dna strand in it's current state.
-
+console.log(dna);
 
 //a) Use the .trim() method to remove the leading and trailing whitespace, then print the result.
-
-console.log(/* Your code here. */);
+let trimDNA = dna.trim();
+console.log(trimDNA);
 
 //b) Change all of the letters in the dna string to UPPERCASE, then print the result.
 
-console.log();
+console.log(trimDNA.toUpperCase());
+dna = trimDNA;
 
 //c) Note that after applying the methods above, the original, flawed string is still stored in dna. To fix this, we need to reassign the changes to back to dna.
 //Apply these fixes to your code so that console.log(dna) prints the DNA strand in UPPERCASE with no whitespace.
